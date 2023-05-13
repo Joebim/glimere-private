0528be184d257d1754e9d9308eea5799f9034a8f
Joebim <josey359@gmail.com>
2023-05-13 21:32:14 +0100

    [FIX] Syntax issue

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from '../global-components/navbar'
+import Navbar from '../global-components/Navbar'
 import { Link } from 'react-router-dom';
 import landingImg from '../assets/glimere-landing.png'
 // import * as THREE from 'three';
