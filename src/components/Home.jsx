a314032782d7d93735e841d7d4f323ab6e092abf
Joebim <josey359@gmail.com>
2023-05-16 04:40:42 +0100

    [ADD] Waitlist modal

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
+import React, { Fragment, useRef, useState } from 'react'
 import Navbar from '../global-components/Navbar'
 import { Link } from 'react-router-dom';
 import landingImg from '../assets/glimere-landing.png'
-// import * as THREE from 'three';
-import { useRef } from 'react'
+// import * as THREE from 'three';=
 import glimere from '../assets/glimere.svg'
 import glimerenew from '../assets/glimerenew.svg'
 import style from "../assets/style.jpg"
@@ -393,7 +392,7 @@ Join us on Glimere to be a part of a community that values inclusivity and celeb
         </Transition.Child>
 
         <div className="fixed inset-0 z-10 overflow-y-auto">
-          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+          <div className="flex min-h-full items-center justify-center p-4 text-center sm:items-center sm:p-0">
             <Transition.Child
               as={Fragment}
               enter="ease-out duration-300"
@@ -403,42 +402,37 @@ Join us on Glimere to be a part of a community that values inclusivity and celeb
               leaveFrom="opacity-100 translate-y-0 sm:scale-100"
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
-              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-                <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+              <Dialog.Panel className="relative max-h-[700px] p-4 overflow-y-visible transform overflow-hidden rounded-lg bg-[#fff5ee] text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                <div className="bg-[#fff5ee] px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
                   <div className="sm:flex sm:items-start">
-                    <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
-                      <ExclamationTriangleIcon className="h-6 w-6 text-red-600" aria-hidden="true" />
-                    </div>
-                    <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
-                      <Dialog.Title as="h3" className="text-base font-semibold leading-6 text-gray-900">
-                        Deactivate account
-                      </Dialog.Title>
-                      <div className="mt-2">
-                        <p className="text-sm text-gray-500">
-                          Are you sure you want to deactivate your account? All of your data will be permanently
-                          removed. This action cannot be undone.
-                        </p>
+                    <form name="contact" method="POST" netlify>
+                    <div class="border-b border-gray-900/10 pb-4">
+                        <h2 class="text-[40px] font-semibold  text-[#9d5c0d]" style={{fontFamily: "Edensor"}}>Get Early Access to Glimere's Exclusive Waitlist!</h2>
+                        <p class="mt-8 text-sm leading-6 text-gray-600">Thank you for your interest in Glimere! Your details will be used to inform you of our epic launch.</p>
+
+                        <div class="mt-10 ">
+                          <div class="sm:col-span-3 mb-4">
+                            <div class="mt-2">
+                              <input type="text" name="first-name" placeholder='Name' id="first-name" autocomplete="given-name" class="block w-full rounded-md border-0 py-1.5 px-4 text-gray-900 shadow-sm  ring-gray-300 placeholder:text-gray-400 ring-[1px] focus:ring-[#9d5c0d] sm:text-sm sm:leading-6"></input>
+                            </div>
+                          </div>
+
+                          <div class="sm:col-span-4">
+                            <div class="mt-2">
+                              <input id="email" name="email" placeholder='Email' type="email" autocomplete="email" class="block w-full rounded-md border-0 py-1.5 px-4 text-gray-900 shadow-sm  ring-gray-300 placeholder:text-gray-400 ring-[1px] focus:ring-[#9d5c0d] sm:text-sm sm:leading-6"></input>
+                            </div>
+                          </div>
+                         
+                        </div>
                       </div>
+                      <div class="mt-6 flex items-center justify-center gap-x-6">
+                      <button type="submit" class="rounded-md bg-[#ed7534] px-24 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">I'm in!</button>
                     </div>
+                  </form>
+                  
                   </div>
                 </div>
-                <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-                  <button
-                    type="button"
-                    className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
-                    onClick={() => setOpen(false)}
-                  >
-                    Deactivate
-                  </button>
-                  <button
-                    type="button"
-                    className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
-                    onClick={() => setOpen(false)}
-                    ref={cancelButtonRef}
-                  >
-                    Cancel
-                  </button>
-                </div>
+           
               </Dialog.Panel>
             </Transition.Child>
           </div>
