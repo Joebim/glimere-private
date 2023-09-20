5158407fd6fc6783a6d2bf08b49017a11e6465c9
Joebim <josey359@gmail.com>
2023-09-20 08:57:51 +0100

    [ADD] home

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,19 @@ import Home from './pages/Home'
 import { Routes } from 'react-router'
 import { Route } from 'react-router'
 import FontLoad from './global-components/Fontload'
+import Navbar from './global-components/Navbar'
+import Services from './pages/Services'
 
 function App() {
 
   FontLoad()
   return (
     <>
+          <Navbar />
+
     <Routes>
-      <Route path='/' element={<Home />} />
+      {/* <Route path='/' element={<Home />} /> */}
+      <Route path='/' element={<Services />} />
     </Routes>
     </>
     
